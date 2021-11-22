cb60c9a0d1f82d1540352783e569958fb3748546
jirenmaa <jirenmaa@gmail.com>
2021-11-22 18:51:54 +0700

    website(activation): improve user experience

--- website/src/modules/auth/activation.vue
+++ website/src/modules/auth/activation.vue
@@ -4,11 +4,16 @@ import { defineComponent, reactive } from 'vue'
 import router from '@/routes'
 import activationService from './services/activation'
 
+import LoadingSpinner from '@/components/shared/LoadingSpinner.vue'
+
 export default defineComponent({
   name: 'Activation',
+  components: {
+    LoadingSpinner
+  },
   setup () {
     const state = reactive({
-      loading: false,
+      loading: true,
       email: '',
       success: false,
       isResetActivation: false
@@ -21,8 +26,11 @@ export default defineComponent({
      * @returns {Promise<void>}
      */
     async function sendNewActivation (): Promise<void> {
-      const errors = await activationService.resetActivation(state.email)
-      state.success = !!errors
+      state.loading = true
+      state.success = false
+      await activationService.resetActivation(state.email)
+      state.success = true
+      state.loading = false
     }
 
     /**
@@ -36,64 +44,64 @@ export default defineComponent({
 
       const response = await activationService.activate(key)
       state.success = response || false
+      state.loading = false
     }
 
     sendActivation()
 
-    return { state, sendActivation, sendNewActivation }
+    return { state, sendNewActivation }
   }
 })
 </script>
 
 <template>
-  <div
-    class="grid place-items-center h-screen"
-    v-if="!state.loading && !state.isResetActivation"
-  >
-    <div class="space-y-4" v-if="state.success">
-      <div class="font-jakarta font-medium capitalize text-8xl">
-        Welcome &#127881;
-      </div>
-      <div class="text-lg space-x-2">
-        <span>Your account has been activated.</span>
-        <router-link to="/login" class="text-blue-500 hover:underline"
-          >login now</router-link
-        >
-      </div>
-    </div>
-    <div class="space-y-4 mx-60" v-else>
-      <div class="font-jakarta leading-tight font-medium text-6xl">
-        It appears that your activation link has expired &#x270B;
-      </div>
-      <div class="text-lg space-x-2">
-        <span>Do you want us to send you a new activation links?</span>
-        <span class="cursor-pointer text-blue-500 hover:underline"
-          @click="(e) => state.isResetActivation = true"
-          >Yes send me a new one</span
-        >
-      </div>
-    </div>
+  <div v-if="state.loading" class="grid place-items-center h-screen">
+    <LoadingSpinner />
   </div>
-  <div class="grid place-items-center h-screen" v-else>
-    <form class="space-y-6" @submit.prevent="sendNewActivation">
-      <div v-if="state.success" class="text-green-500 w-96">
-        We have send new account activation link to your email! 🎉
+  <div v-else>
+    <div class="grid place-items-center h-screen" v-if="!state.isResetActivation">
+      <div class="space-y-4" v-if="state.success">
+        <div class="font-jakarta font-medium capitalize text-8xl">Welcome &#127881;</div>
+        <div class="text-lg space-x-2">
+          <span>Your account has been activated.</span>
+          <router-link to="/login" class="text-blue-500 hover:underline">login now</router-link>
+        </div>
       </div>
-      <div class="flex flex-col space-y-4">
-        <label for="email" class="capitalize font-medium text-xl">your email</label>
-        <input
-          class="w-96 bg-dark text-base rounded-sm px-4 p-3"
-          type="email"
-          name="email"
-          placeholder="account@gmail.com"
-          v-model="state.email"
-        />
+      <div class="space-y-4 mx-60" v-else>
+        <div
+          class="font-jakarta leading-tight font-medium text-6xl"
+        >It appears that your activation link has expired &#x270B;</div>
+        <div class="text-lg space-x-2">
+          <span>Do you want us to send you a new activation links?</span>
+          <span
+            class="cursor-pointer text-blue-500 hover:underline"
+            @click="(e) => state.isResetActivation = true"
+          >Yes send me a new one</span>
+        </div>
       </div>
-      <div class="flex flex-col space-y-4">
-        <button
-          class="w-96 bg-dark hover:bg-black border border-dark-grey rounded-sm capitalize text-base px-4 p-3"
-        >reset activation</button>
-      </div>
-    </form>
+    </div>
+    <div class="grid place-items-center h-screen" v-else>
+      <form class="space-y-6" @submit.prevent="sendNewActivation">
+        <div
+          v-if="state.success"
+          class="text-green-500 w-96"
+        >We have send new account activation link to your email! 🎉</div>
+        <div class="flex flex-col space-y-4">
+          <label for="email" class="capitalize font-medium text-xl">your email</label>
+          <input
+            class="w-96 bg-dark text-base rounded-sm px-4 p-3"
+            type="email"
+            name="email"
+            placeholder="account@gmail.com"
+            v-model="state.email"
+          />
+        </div>
+        <div class="flex flex-col space-y-4">
+          <button
+            class="w-96 bg-dark hover:bg-black border border-dark-grey rounded-sm capitalize text-base px-4 p-3"
+          >reset activation</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
